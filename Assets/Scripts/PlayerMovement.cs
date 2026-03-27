688b53c3c82e57a1b01174f38d2dddf4f16e9b2a
Melonmancer <huggos100@gmail.com>
2026-03-27 20:04:33 +0800

    Basic Player and Camera Movement
    + Imports the Unity packages 'Input System' and 'Cinemachine'
    + Adds a new prefabs folder with basic player and camera prefabs
    + Adds a new scene 'MovementTest'
    + Adds a new script 'PlayerMovement' which currently allows for walking around
    + Camera prefab is set up with Cinemachine to follow the player character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //speed manages how fast the player moves at base - manually modify this to speed up or slow down the player character
+    //rotationSpeed manages how quickly the player object rotates to match the direction the camera is pointing
+    private float speed = 10.0f;
+    private float rotationSpeed = 15.0f;
+
+
+    private Vector3 direction;
+    private Vector3 viewDirection;
+
+    //Stores references to the player and camera objects in the same scene
+    //NOTE: If you are making a new scene, you'll need to link these manually from the inspector
+    public GameObject playerObject;
+    public Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        ProcessPlayerMovement();        
+    }
+
+
+
+    //ProcessPlayerMovement checks if the player is inputting anything, and updates the player object's position and facing
+    private void ProcessPlayerMovement()
+    {
+        //Horizontal checks for A, D, Left and Right arrow keys
+        float horizontal = Input.GetAxis("Horizontal");
+        //Vertical checks for W, S, Up and Down arrow keys
+        float vertical = Input.GetAxis("Vertical");
+
+        //While the player is moving they rotate to face the direction the camera is facing
+        if(horizontal != 0.0f || vertical != 0.0f)
+        {
+            UpdatePlayerDirection();
+        }
+
+        //Creates a new direction based on which keys the player is pressing
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //Moves the player object in the new direction
+        playerObject.transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+
+
+    //UpdatePlayerDirection interpolates the player object to face towards the direction the camera is facing
+    private void UpdatePlayerDirection()
+    {
+        //Subtracting the camera's position along the X and Z axes from the player's position yields the direction the camera is facing (excluding the camera's tilt up or down)
+        viewDirection = playerObject.transform.position - new Vector3(playerCamera.transform.position.x, playerObject.transform.position.y, playerCamera.transform.position.z);
+
+        //Interpolates between the direction the player is currently facing and the direction the camera is facing, and sets the player to face this new direction
+        playerObject.transform.forward = Vector3.Slerp(playerObject.transform.forward, viewDirection.normalized, rotationSpeed * Time.deltaTime);
+    }
+}
