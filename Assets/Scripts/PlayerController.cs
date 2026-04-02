316f962fa31c4b6da7899ac7acbf130b09ece1c8
Melonmancer <huggos100@gmail.com>
2026-04-02 16:43:07 +0800

    Tidying and fixing player movement
    - Merges contents of Player.cs collision script into PlayerController.cs
    ~ PlayerController now takes player inputs in Update
    ~ Tidied PlayerController script
    ~ Added additional platforms to MovementTest scene
    + PlayerController now allows 'coyote time' for player to jump after leaving a platform
    + Fixed bug preventing jumping until the player stops moving after landing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,14 @@ public class PlayerController : MonoBehaviour
     private float rotationSpeed = 1.5f;
     
 
+    private float horizontalInput;
+    private float verticalInput;
+    private float jumpInput;
+
+    //Controls player's ability to jump slightly after leaving a platform
+    private bool coyoteTime = false;
+    private float coyoteWindow = 0f;
+    [SerializeField] private float coyoteMax = 8f;
 
     private Vector3 direction;
     private Vector3 viewDirection;
@@ -33,59 +41,82 @@ public class PlayerController : MonoBehaviour
         playerRigidbody = playerObject.GetComponent<Rigidbody>();
     }
 
-
+    //Update called each frame update, collects player inputs
     void Update()
     {
-
+        GetPlayerInputs();
     }
 
-    //NOTE: FixedUpdate to solve for some collision issues, if too janky switch back to Update and add * 'Time.deltaTime to all movements
-    //Update is called once per frame
+    //FixedUpdate should be used for all rigidbody movement and collision work
     void FixedUpdate()
     {
         ProcessPlayerMovement();        
     }
 
+    //Collects input data from the player
+    void GetPlayerInputs()
+    {
+        //Horizontal checks for A, D, Left and Right arrow keys
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Vertical checks for W, S, Up and Down arrow keys
+        verticalInput = Input.GetAxis("Vertical");
 
+        jumpInput = Input.GetAxis("Jump");
+    }
+
+    //Resets input data
+    void ResetPlayerInputs()
+    {
+        horizontalInput = 0f;
+        verticalInput = 0f;
+        jumpInput = 0f;
+    }
 
     //ProcessPlayerMovement checks if the player is inputting anything, and updates the player object's position and facing
     private void ProcessPlayerMovement()
     {
 
-
-        //Horizontal checks for A, D, Left and Right arrow keys
-        float horizontal = Input.GetAxis("Horizontal");
-        //Vertical checks for W, S, Up and Down arrow keys
-        float vertical = Input.GetAxis("Vertical");
-
          //While the player is moving they rotate to face the direction the camera is facing
-        if(horizontal != 0.0f || vertical != 0.0f)
+        if(horizontalInput != 0.0f || verticalInput != 0.0f)
         {
             UpdatePlayerDirection();
         }
 
         //Creates a new direction based on which keys the player is pressing
         var verticalRotation = Quaternion.Euler(0,  playerObject.transform.rotation.eulerAngles.y, 0);
-        direction = (verticalRotation * new Vector3(horizontal, 0f, vertical)).normalized;
+        direction = (verticalRotation * new Vector3(horizontalInput, 0f, verticalInput)).normalized;
 
-        
-        
         //Moves the player object in the new direction
         //playerObject.transform.Translate(direction * speed);
         var prevVerticalVelocity = playerRigidbody.velocity.y;
         playerRigidbody.velocity = direction * speed; 
         playerRigidbody.velocity += prevVerticalVelocity * Vector3.up;
 
-        if (Input.GetAxis("Jump") > 0 && grounded)
+
+        if (jumpInput > 0 && (grounded || coyoteTime))
         {
-            SetGrounded(false);
+            grounded = false;
+            coyoteTime = false;
+            coyoteWindow = 0f;           
             //Debug.Log("Jumping!");
             playerRigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
         }
 
-       
-    }
 
+        //coyoteTime allows the player to jump immediately after leaving a platform - raise coyoteMax for a bigger window
+        if(coyoteTime)
+        {
+            coyoteWindow += 1f;
+            //Debug.Log(coyoteWindow);
+        }
+        if(coyoteTime && (coyoteWindow >= coyoteMax))
+        {
+            coyoteTime = false;
+            coyoteWindow = 0f;
+        }
+
+       ResetPlayerInputs();
+    }
 
 
     //UpdatePlayerDirection interpolates the player object to face towards the direction the camera is facing
@@ -99,13 +130,35 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    public void SetGrounded(bool boolean)
+
+    //Called by the collider component
+    //If the player has no vertical velocity (ie. not falling or rising) and collides with something, reset their jump
+    void OnCollisionStay()
     {
-        grounded = boolean;
+        if(!grounded)
+        {
+            if(playerRigidbody.GetPointVelocity(this.transform.position).y == 0f)
+            {
+                //Debug.Log("Grounded!");
+                grounded = true;
+            }
+        }
     }
 
-    public bool GetGrounded()
+    //Called by the collider component
+    //Sets the player's falling state if they are moving and leave a collision
+    void OnCollisionExit()
     {
-        return grounded;
+        if(grounded)
+        {
+            if(playerRigidbody.GetPointVelocity(this.transform.position) != Vector3.zero)
+            {
+                //Debug.Log("Falling!");
+                grounded = false;
+                coyoteTime = true;
+            }            
+        }
     }
+
+
 }
