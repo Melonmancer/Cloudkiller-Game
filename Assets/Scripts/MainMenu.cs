88e0b8b4a8f661bf31a1d1539141a17921ac31c3
jenlinn <mullan.jen@gmail.com>
2026-04-29 21:39:55 +0800

    Implementing Menus
    Added MainMenu scene (and controls panel)
    
    Added Pause screen to Level1LayoutScene
    
    Added MainMenu and Level1Layout scenes to Build Settings in 0 and 1 place respectively.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject controlsPanel;
+    public GameObject mainMenuPanel;
+    
+    void Start()
+    {
+        controlsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level1Layout");
+    }
+
+    public void OpenControls()
+    {
+        Time.timeScale = 1f;
+        controlsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        
+    }
+
+    public void ExitControls()
+    {
+        Time.timeScale = 1f;
+        controlsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        
+    }
+
+    public void ExitGame()
+    {
+        //ApplicationException.Quit();
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+
+
+{
+
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+
+    }
+
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        //ApplicationException.Quit();
+    }
+
+}
