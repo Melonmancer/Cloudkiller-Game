06a3d258a741378f5c33de75bbb27900a873542b
Melonmancer <huggos100@gmail.com>
2026-05-07 00:57:34 +0800

    'Snitch' Angel base code
    + Implements basic prefab and script for The Snitch

---
+++ Assets/Scripts/Angels/SnitchAngel/SnitchAngel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SnitchAngel : MonoBehaviour
+{
+
+    //TO DO:
+    //Copy small angel behaviour in here, edit to remove chasing behaviour
+    //Replace chasing behaviour with alerting Seraphim
+    //Find function to get the Seraphim inside the scene
+    //Code spawner to set all values for this
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
